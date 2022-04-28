52223db03a9c00bf597da7d9b9dceb0944b0c418
Christopher <rezielsaw@gmail.com>
2022-04-28 22:35:15 +1000

    Task 001

---
+++ seminar001/Task001/Program.cs
@@ -0,0 +1,6 @@
+﻿// вывести квадрат числа
+Console.WriteLine("Введите число ,");
+double number = Double.Parse(Console.ReadLine());
+double squre = (number * number);
+Console.WriteLine("Квадрат числа равен ");
+Console.WriteLine(squre);
