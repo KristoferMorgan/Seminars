eacfe8209a56bbe2be73bfa496c1193472eb1b8c
Christopher <rezielsaw@gmail.com>
2022-04-29 17:24:15 +1000

    Task005

---
+++ seminar001/Task005/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трехзначного числа
+int remainder = 0;
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+remainder = number % 10;
+Console.Write("Последняя цифра вашего числа ");
+Console.WriteLine(remainder);
