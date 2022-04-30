2941c871bad359919f363239a43ab5e01f64792b
Christopher <rezielsaw@gmail.com>
2022-04-30 21:43:31 +1000

    HomeWork 1.4

---
+++ seminar001/HomeWork/Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать все четные числа от 1 до N
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+Console.WriteLine("Четные числа от 1 до N :");
+while(N + 1 > count)
+{
+ if (count % 2 == 0) 
+  Console.WriteLine(count);
+count = count + 1; 
+}   
