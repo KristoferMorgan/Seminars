3d1939ec8d404e1d7ed7a69c6c15fbbbc6aca2fb
Christopher <rezielsaw@gmail.com>
2022-05-01 01:45:20 +1000

    Homework 1.7

---
+++ seminar001/HomeWork/Task007/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трехзначниго числа
+int Number = new Random().Next(100,999);
+Console.WriteLine("Ваше случайное число");
+Console.WriteLine(Number);
+int Number_2 = Number % 10;
+int Number_1 = Number /10/10;
+Console.Write("Убрали среднюю цифру,получилось = ");
+Console.Write(Number_1);
+Console.Write(Number_2);
