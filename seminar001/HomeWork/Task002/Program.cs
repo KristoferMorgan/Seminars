3bc28862650fa4926286d47838bef5c95ff3fb78
Christopher <rezielsaw@gmail.com>
2022-04-30 14:04:03 +1000

    HomeWork002

---
+++ seminar001/HomeWork/Task002/Program.cs
@@ -0,0 +1,131 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите число:");
+int day = int.Parse(Console.ReadLine());
+string day_identifier = "Некорректное число";
+if (day == 1 )
+{
+  day_identifier = "Ваш день понедельник." ;
+}
+if (day == 2 )
+{
+  day_identifier = "Ваш день вторник." ;
+}
+if (day == 3 )
+{
+  day_identifier = "Ваш день среда." ;
+}
+if (day == 4 )
+{
+  day_identifier = "Ваш день четверг." ;
+}
+if (day == 5 )
+{
+  day_identifier = "Ваш день пятница." ;
+}
+if (day == 6 )
+{
+  day_identifier = "Ваш день суббота." ;
+}
+if (day == 7 )
+{
+  day_identifier = "Ваш день воскресенье." ;
+}
+if (day == 8 )
+{
+  day_identifier = "Ваш день понедельник." ;
+}
+if (day == 9 )
+{
+  day_identifier = "Ваш день вторник." ;
+}
+if (day == 10 )
+{
+  day_identifier = "Ваш день среда." ;
+}
+if (day == 11 )
+{
+  day_identifier = "Ваш день четверг." ;
+}
+if (day == 12 )
+{
+  day_identifier = "Ваш день пятница." ;
+}
+if (day == 13 )
+{
+  day_identifier = "Ваш день суббота." ;
+}
+if (day == 14 )
+{
+  day_identifier = "Ваш день воскресенье." ;
+}
+if (day == 15 )
+{
+  day_identifier = "Ваш день понедельник." ;
+}
+if (day == 16 )
+{
+  day_identifier = "Ваш день вторник." ;
+}
+if (day == 17 )
+{
+  day_identifier = "Ваш день среда." ;
+}
+if (day == 18 )
+{
+  day_identifier = "Ваш день четверг." ;
+}
+if (day == 19 )
+{
+  day_identifier = "Ваш день пятница." ;
+}
+if (day == 20 )
+{
+  day_identifier = "Ваш день суббота." ;
+}
+if (day == 21 )
+{
+  day_identifier = "Ваш день воскресенье." ;
+}
+if (day == 22)
+{
+  day_identifier = "Ваш день понедельник." ;
+}
+if (day == 23 )
+{
+  day_identifier = "Ваш день вторник." ;
+}
+if (day == 24 )
+{
+  day_identifier = "Ваш день среда." ;
+}
+if (day == 25)
+{
+  day_identifier = "Ваш день четверг." ;
+}
+if (day == 26)
+{
+  day_identifier = "Ваш день пятница." ;
+}
+if (day == 27)
+{
+  day_identifier = "Ваш день суббота." ;
+}
+if (day == 28)
+{
+  day_identifier = "Ваш день воскресенье." ;
+}
+if (day == 29)
+{
+  day_identifier = "Ваш день понедельник." ;
+}
+if (day == 30)
+{
+  day_identifier = "Ваш день вторник." ;
+}
+if (day == 31)
+{
+  day_identifier = "Ваш день среда." ;
+}
+
+Console.WriteLine(day_identifier);
+// Недопонял как писать сложное условие,поэтому сделал так.Хотел разобрать на семинаре.
