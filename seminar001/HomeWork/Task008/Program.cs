baa2aed72e33dba7a3dcdc2d87eecbca07cb78cd
Christopher <rezielsaw@gmail.com>
2022-05-01 02:34:04 +1000

    Homework 1.8

---
+++ seminar001/HomeWork/Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить,кратно ли число заданному,если нет,вывести остаток
+int Number = new Random().Next(0,10000);
+Console.Write( "Введите число: ");
+int Number_1 = int.Parse(Console.ReadLine());
+int remainder = Number % Number_1;
+Console.WriteLine("Ваше случайное число");
+Console.WriteLine(Number);
+if(Number % Number_1 == 0 )
+  Console.WriteLine("Число кратно заданному");
+  else
+    Console.Write("Число не кратно заданному,его остаток = ");
+    Console.WriteLine(remainder);
