725560c7f1c78a7d10c40c8a726f58756342ee52
Christopher <rezielsaw@gmail.com>
2022-05-01 03:02:00 +1000

    Homework 1.9

---
+++ seminar001/HomeWork/Task009/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить,что ее нет
+int Number = new Random().Next(0,10000);
+Console.Write("Ваше случайное число ");
+Console.WriteLine(Number);
+int Number_3 = Number / 100 % 10;
+if(Number / 100 != 0)
+{
+  Console.Write("Третья цифра числа ");
+    Console.WriteLine(Number_3);
+    }
+  else
+   {
+       Console.WriteLine("У числа нет третьей цифры");
+} 
