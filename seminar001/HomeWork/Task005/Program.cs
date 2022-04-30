29f2c4d2ca1c6fb44604b7cce25e9bde8202cfee
Christopher <rezielsaw@gmail.com>
2022-04-30 22:41:36 +1000

    Homework 1.5

---
+++ seminar001/HomeWork/Task005/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трехзначного числа
+Console.WriteLine("Введите трехзначное число,что бы найти вторую цифру");
+int Number = int.Parse(Console.ReadLine());
+int Number_2 = Number / 10 % 10;
+Console.Write("Вторая цифра вашего числа :");
+Console.WriteLine(Number_2);
+
