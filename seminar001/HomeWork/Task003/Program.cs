d49e0700652b7a72b9c1ad45f6693061a73f914f
Christopher <rezielsaw@gmail.com>
2022-04-30 20:13:50 +1000

    homework 1.3

---
+++ seminar001/HomeWork/Task003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0) 
+  Console.WriteLine("Ваше число четное");
+else 
+    Console.WriteLine("Веше число нечетное");
