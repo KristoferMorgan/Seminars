8648e2e66ce3f5d27e86fb2cac64dae8eced966d
Christopher <rezielsaw@gmail.com>
2022-05-13 12:26:59 +1000

    task003_3

---
+++ seminar002/Task003/Program.cs
@@ -0,0 +1,8 @@
+﻿// Узнать является ли пятизначное число палиндром
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+  Console.WriteLine("число является палиндромом");
+else
+  Console.WriteLine("Число не является палиндром");
+
