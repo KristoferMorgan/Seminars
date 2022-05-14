799afe1394087cd913fcf6b32ca7ab0e50785359
Christopher <rezielsaw@gmail.com>
2022-05-14 11:19:02 +1000

    Seminar 2 Homework task 2

---
+++ seminar002/HomeWork/Task002/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Enter Number One");
+int  number_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Number Two");
+int number_2 = int.Parse(Console.ReadLine());
+if (number_1 * number_1 == number_2)
+{
+  Console.WriteLine("the first number is the square of the second number");
+}
+else
+  Console.WriteLine("the first number is not the square of the second number");
