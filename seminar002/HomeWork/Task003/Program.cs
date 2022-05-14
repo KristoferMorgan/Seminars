df39024dfba832c44c1802c58c5a89346df9547a
Christopher <rezielsaw@gmail.com>
2022-05-14 18:09:39 +1000

    Seminar 2 HomeWork Task 3

---
+++ seminar002/HomeWork/Task003/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("enter the worm number to find out its range of coordinates :  ");
+int QuarterNumber = int.Parse(Console.ReadLine());
+if (QuarterNumber == 1)
+{
+  Console.WriteLine("range of your coordinates: ");
+  Console.WriteLine("x = from zero to infinity");
+  Console.WriteLine("y = from zero to infinity");
+}
+else if (QuarterNumber == 2)
+{
+    Console.WriteLine("range of your coordinates: ");
+    Console.WriteLine("x = from -infinity to zero");
+    Console.WriteLine("y = from zero to infinity");
+}
+else if (QuarterNumber == 3)
+{
+    Console.WriteLine("range of your coordinates: ");
+    Console.WriteLine("x = from -infinity to zero");
+    Console.WriteLine("y = - infinity to zero");
+}
+else if (QuarterNumber == 4)
+{
+    Console.WriteLine("range of your coordinates: ");
+    Console.WriteLine("x = from zero to infinity");
+    Console.WriteLine("y = from -infinity to zero");
+}
+else
+{
+    Console.WriteLine("The number of such a quarter of the plane does not exist");
+    Console.WriteLine("Repeat input");
+    }
