63cacf629f656ca007a84bcd574c41196729a982
Christopher <rezielsaw@gmail.com>
2022-05-14 10:49:06 +1000

    Seminar 2 Homework Task 1

---
+++ seminar002/HomeWork/Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("enter the number : ");
+int number = int.Parse(Console.ReadLine());
+if (number % 7 == 0 || number % 6 == 0 )
+{
+  Console.WriteLine("This day is a day off!!!");
+}
+else if (number < 0 || number >31 )
+{
+  Console.WriteLine("your number is not showing the day of the month");
+}
+else 
+{
+  Console.WriteLine("This day is working");
+}  
--- seminar002/Task001/Program.cs
+++ seminar002/Task001/Program.cs
@@ -2,6 +2,10 @@
 Console.WriteLine("Введите число ");
 int number = int.Parse(Console.ReadLine());
 if (number % 7 == 0 && number % 23 == 0)
-   Console.WriteLine ("Ваше число кратно 7 и 23");
-else
+{
+   Console.WriteLine ("Ваше число кратно 7 и 23"); 
+}
+else 
+{
    Console.WriteLine("Ваше число не кратно 7 и 23");
+}
