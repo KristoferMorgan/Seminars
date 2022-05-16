81d57bd1cc344c4f6fa6f48a440a3a6234bcc66f
Christopher <rezielsaw@gmail.com>
2022-05-16 19:13:09 +1000

    Seminar 3 HomeWork Task 1-4

---
+++ seminar003/task002/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти сумму чисел от 1 до А
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+int result = 0;
+while(count <= number)
+{
+    result = result + count;
+    
+    count=count + 1;
+}
+Console.WriteLine(result);
+//n(n+1)\2
---
+++ seminar003/task004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задать массив из 8 элементов и вывести их на экран");
+
+int[] array = new int[8];
+
+for (int i=0; i<8; i++)
+    array[i] =  new Random().Next(0,100);
+    
+int count = 1;
+
+foreach (int k in array)
+{
+    Console.WriteLine($"{count} элемент массива - {k}");
+    count++;
+}
