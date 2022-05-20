ad473986c7349714301ecd5778a8e7498f49c966
Christopher <rezielsaw@gmail.com>
2022-05-20 14:52:50 +1000

    Seminar 3 HomeWork Task 4

--- seminar003/HomeWork/Task004/Program.cs
+++ seminar003/HomeWork/Task004/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("this is a program for raising a number to a power");
+Console.WriteLine("enter a short number to be raised to a power:");
+int A = int.Parse(Console.ReadLine()??"");
+Console.WriteLine("Enter the power to which you want to raise the number:");
+int B = int.Parse(Console.ReadLine()??"");
+int Variable = A;
+for(int i = 1;i < B;i++)
+    Variable = Variable * A;
+
+Console.WriteLine($"{A} to the power of {B} = {Variable}");
