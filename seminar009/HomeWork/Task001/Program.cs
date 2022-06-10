142319de6cd45ad7a4ecd5493bc70f7000b1a11c
Christopher <rezielsaw@gmail.com>
2022-06-10 13:47:02 +1000

    seminar009 Home Work 1-2

---
+++ seminar009/HomeWork/Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.Clear();
+void NumberMtoN(int NumberTo, int NumberDo)
+{
+    if(NumberDo + 1 > NumberTo)
+        if(NumberDo > 0 )
+        {
+          Console.WriteLine(NumberTo);
+          NumberTo++;
+          NumberMtoN(NumberTo,NumberDo);
+        }
+        else
+            {
+              Console.WriteLine("$number {NumberTo} is not a natural number");
+              NumberTo++;
+              NumberMtoN(NumberTo,NumberDo);     
+            }   
+    else
+      return;
+}
+int n = 10;
+int m = 2;
+NumberMtoN(m , n );
+
---
+++ seminar009/HomeWork/task02/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+Console.Clear();
+int NumberSum(int NumberTo,int NumberDo)
+{
+  int sum = NumberTo;
+  if(NumberTo < NumberDo)
+    return sum = sum + NumberSum(NumberTo + 1,NumberDo);
+  else
+     return sum;
+}
+int m = 1;
+int n = 10;
+NumberSum(m,n);
+Console.WriteLine($"the sum of the elements from m to n is {NumberSum(m,n)}");
